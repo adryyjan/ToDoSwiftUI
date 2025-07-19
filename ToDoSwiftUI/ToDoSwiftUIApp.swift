d6cb5f48852fc82efa30ff0c5ee54f835124bc74
Adrian Mazek <a6r14n@gmail.com>
2025-07-19 19:56:03 +0200

    Initial Commit

---
+++ ToDoSwiftUI/ToDoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftUIApp.swift
+//  ToDoSwiftUI
+//
+//  Created by Adrian Mazek on 19/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
